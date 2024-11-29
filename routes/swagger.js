b0ab40a7d526b38789b0acec715d9b55a14d0f84
divineProjects <divinejigu@gmail.com>
2024-11-29 03:11:04 +0200

    week3 final update

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 
-router.use("/stockApi", swaggerUi.serve);
-router.get("/stockApi", swaggerUi.setup(swaggerDocument));
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
 
